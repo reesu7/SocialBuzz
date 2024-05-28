290deebc96f751a4c4ca7ef181f8a2b26d86bb03
rishPR <rishurawat1703@gmail.com>
2024-05-28 18:59:06 +0530

    changed ui

--- src/app.py
+++ src/app.py
@@ -1,20 +1,15 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from prediction import preprocess, load_models, predict
 from datafetch import fetch_twitter_data
 
-# Initialize the Flask app
 app = Flask(__name__)
 
-# Load the models
 vectoriser, LRmodel = load_models()
 
-# Redirects to the landing page
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Redirects to the page where user inputs data
-# and gets the sentiment of the input text
 @app.route('/typing', methods=['GET', 'POST'])
 def typing():
     if request.method == 'GET':
@@ -30,21 +25,19 @@ def typing():
             error = str(e)
             return render_template('typing.html', error=error)
 
-# Redirects to the page where user inputs a hashtag
-# and gets the sentiment of the tweets related to the hashtag
 @app.route('/live', methods=['GET', 'POST'])
 def live():
     if request.method == 'GET':
-        return render_template('live.html')
+        return render_template('newTwitter.html')
     elif request.method == 'POST':
         try:
-            text = request.form['text']
-            texts = fetch_twitter_data(text)
+            keyword = request.json['text']
+            texts = fetch_twitter_data(keyword)
             if texts:
-                total_positive_count = 0
-                total_negative_count = 0
                 positive_texts = [] 
                 negative_texts = []  
+                total_positive_count = 0
+                total_negative_count = 0
                 vectoriser, LRmodel = load_models()
                 for text in texts:
                     processed_text = preprocess([text])  
@@ -59,18 +52,20 @@ def live():
                 total_texts = len(texts)
                 total_positive_percentage = (total_positive_count / total_texts) * 100
                 total_negative_percentage = (total_negative_count / total_texts) * 100
-
-                return render_template('live.html', 
-                                       total_positive_percentage=total_positive_percentage,
-                                       total_negative_percentage=total_negative_percentage,
-                                       positive_texts=positive_texts,
-                                       negative_texts=negative_texts)
+                return jsonify({
+                    'positive': total_positive_percentage,
+                    'negative': total_negative_percentage,
+                    'positive_texts':positive_texts,
+                    'negative_texts':negative_texts
+                })
             else:
-                return render_template('live.html', error="No tweets found for the given hashtag.")
+                return jsonify({"error": "No tweets found for the given hashtag."})
         except Exception as e:
-            error = str(e)
-            return render_template('live.html', error=error)
-
+            return jsonify({"error": str(e)}) 
+        
+@app.route('/result')
+def result():
+    return render_template('newResults.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
